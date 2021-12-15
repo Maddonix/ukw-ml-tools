a360821230ad4c7a02f3be26a62644dc424eed55
Thomas Lux <tlux14@googlemail.com>
2021-12-15 16:55:06 +0100

    asd

--- src/ukw_ml_tools/classes/requests/train.py
+++ src/ukw_ml_tools/classes/requests/train.py
@@ -2,16 +2,17 @@ from pydantic import BaseModel, validator
 from ..config import Configuration, AiLabelConfig
 from ..train_data import TrainDataDb, TrainData
 from typing import Optional
+from pathlib import Path
 
 
 class TrainRequest(BaseModel):
-    cfg: Configuration
-    ai_config: AiLabelConfig
+    name: Path
+    base_path_models: Path
     train_data_db: TrainDataDb
     train_data: Optional[TrainData]
 
     @validator("train_data", always=True)
     def set_train_data(cls, v, values):
         v = values["train_data_db"]
-        v = TrainData(**v)
+        v = TrainData(**v.to_dict_intern())
         return v
